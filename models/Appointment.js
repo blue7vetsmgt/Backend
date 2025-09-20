1aeffe932e3cf84669563feb56eb301b88a76cc2
Vaishnavi <blue7vetsmgt@gmail.com>
2025-09-20 20:37:26 +0530

    fix for appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const AppointmentSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   serviceId: { type: String, required: true }, // e.g. "1" for Home Vet Visit
